136e53942ee9f0c0fe8c0d9fc7d7b61ec8e661fc
Marius Treitz <marius.treitz@gmail.com>
2022-08-03 00:24:47 +0200

    fix(deps): remove console.log dependency

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var util = require('util'),
-	whois = require('whois')
-	log = console.log.bind(console),
+	whois = require('whois'),
 	parseRawData = require('./parse-raw-data.js')
 
 var lookup = util.promisify(whois.lookup);
 
-module.exports = async function(domain, options){
+module.exports = async function (domain, options) {
 
-	var rawData = await lookup(domain, options || {})	
+	var rawData = await lookup(domain, options || {})
 
 	var result = {};
 
-	if ( typeof rawData === 'object' ) {
-		result = rawData.map(function(data) {
+	if (typeof rawData === 'object') {
+		result = rawData.map(function (data) {
 			data.data = parseRawData(data.data);
 			return data;
 		});
